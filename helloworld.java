02d0305a9d3457b94e8b6a5289a28f4380b2cd28
rohit <kalra.rohit01@gmail.com>
2026-01-26 17:00:20 +0530

    first commit

---
+++ helloworld.java
@@ -0,0 +1,20 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class helloworld {
+    public static void main(String[] args) {
+        // Information about 26th January
+        LocalDate date = LocalDate.of(2026, 1, 26);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE, MMMM d, yyyy");
+        
+        System.out.println("=== 26th January Information ===");
+        System.out.println("Date: " + date.format(formatter));
+        System.out.println("Day: " + date.getDayOfWeek());
+        System.out.println("Month: " + date.getMonth());
+        System.out.println("Year: " + date.getYear());
+        System.out.println("\nSignificance:");
+        System.out.println("26th January is celebrated as Republic Day in India.");
+        System.out.println("It commemorates the adoption of the Indian Constitution in 1950.");
+        System.out.println("This is a national holiday and marks India's transition to a sovereign republic.");
+    }
+}
